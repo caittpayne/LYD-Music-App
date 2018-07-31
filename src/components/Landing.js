ef99e67101212509b8a84a3b72d3dfba660a176f
Cait Payne <caitpayne@outlook.com>
2018-07-31 11:41:36 -0600

    Added HTML to Landing Page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 const Landing = () => (
   <section className='landing'>
-    Landing page will go here
+    <h1 className='hero-title'>Turn the music up!</h1>
+    <section className='selling-points'>
+      <div className='point'>
+        <h2 className='point-title'>Choose your music</h2>
+        <p className='point-description'>The world is full of music; why should you have to listen to music that someone else chose?</p>
+      </div>
+      <div className='point'>
+        <h2 className='point-title'>Unlimited, streaming, ad-free</h2>
+        <p className='point-description'>No arbitrary limits. No distractions.</p>
+      </div>
+      <div className='point'>
+        <h2 className='point-title'>Mobile enabled</h2>
+        <p className='point-description'>Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+      </div>
+    </section>
   </section>
 );
 
