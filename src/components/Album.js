5db76ca75c7fd10cfef34986b2b11495f0a4c10c
Cait Payne <caitpayne@outlook.com>
2018-07-31 15:24:25 -0600

    Added additional information on Album page - checkpoint complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Album extends Component {
+  constructor(props) {
+    super(props);
+
+    const album = albumData.find( album => {
+      return album.slug === this.props.match.params.slug
+    });
+
+    this.state = {
+      album: album
+    };
+  }
   render() {
     return (
       <section className='album'>
-        {this.props.match.params.slug} Album will go here
+        <section id='album-info'>
+          <img id='album-cover-art' src={this.state.album.albumCover} alt={this.state.album.title}/>
+          <div className='album-details'>
+            <h1 id='album-title'>{this.state.album.title}</h1>
+            <h2 className='artist'>{this.state.album.artist}</h2>
+            <div id='release-info'>{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id='song-list'>
+          <colgroup>
+            <col id='song-number-column' />
+            <col id='song-title-column' />
+            <col id='song-duration-column' />
+          </colgroup>
+          <tbody>
+          </tbody>
+        </table>
       </section>
     );
   }
