925cab244c02ca36c78c0993e991b741bcfe6d37
Cait Payne <caitpayne@outlook.com>
2018-07-31 17:10:53 -0600

    Added play/pause functionality - checkpoint complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,9 +10,43 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
   }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if(this.state.isPlaying && isSameSong === true) {
+      console.log('pause');
+      this.pause();
+    }
+    else {
+      if (!isSameSong) { this.setSong(song); }
+      console.log('play');
+      this.play();
+    }
+  }
+
   render() {
     return (
       <section className='album'>
@@ -33,7 +67,7 @@ class Album extends Component {
           <tbody>
           {
             this.state.album.songs.map((song, index) =>
-                <tr key={index}>
+                <tr key={index} className='song' onClick={() => this.handleSongClick(song)}>
                   <td>{index + 1}</td>
                   <td>{song.title}</td>
                   <td>{song.duration} secs</td>
